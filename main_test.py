30cb80002a0582e6fa3f04bf0175fd8d5a833c13
Yuji Yabe <yayuji@gmail.com>
2019-08-02 16:38:33 +0900

    unit test sample

--- main_test.py
+++ main_test.py
@@ -1,24 +1,25 @@
-# Copyright 2018 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
 
 import main
 
 
-def test_index():
-    main.app.testing = True
-    client = main.app.test_client()
+class TestFlaskHello(unittest.TestCase):
 
-    r = client.get('/')
-    assert r.status_code == 200
-    assert 'Hello World' in r.data.decode('utf-8')
+    def setUp(self):
+        main.app.testing = True
+        self.app = main.app.test_client()
+
+    def test_get(self):
+        response = self.app.get('/')
+        assert response.status_code == 200
+        # assert response.data == 'Hello, World!'
+        assert 'Hello, World!' in response.data.decode('utf-8')
+        # print(response.data.decode('utf-8'))
+        print()
+
+
+if __name__ == '__main__':
+    unittest.main()
